bd7e54c31367471515afff41978465ca1731505a
Jonathan Cameron <jmcameron@gmail.com>
2011-02-20 20:27:30 +0000

    Updates to get component installing.

---
+++ attachments_component/attachments.install.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ */
+
+class Com_AttachmentsInstallerScript {
+
+	function install($parent) {
+		echo 'Attachment component installed!';
+	}
+
+	function uninstall($parent) {
+	}
+
+	function update($parent) {
+	}
+
+	function preflight($type, $parent) {
+	}
+
+	function postflight($type, $parent) {
+	}
+
+}
