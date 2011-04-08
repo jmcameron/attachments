5cf04fb4a6b4aa1b5351d64cebd0c57ee1337b99
Jonathan Cameron <jmcameron@gmail.com>
2011-04-08 21:53:16 +0000

    New main controller entry point.

---
+++ attachments_component/admin/attachments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Attachments component
+ *
+ * @package Attachments
+ * @subpackage Attachments_Component
+ *
+ * @copyright Copyright (C) 2007-2011 Jonathan M. Cameron, All Rights Reserved
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @link http://joomlacode.org/gf/project/attachments/frs/
+ * @author Jonathan M. Cameron
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+// Access check.
+if (!JFactory::getUser()->authorise('core.manage', 'com_attachments')) {
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+ 
+// Import the controller library
+jimport('joomla.application.component.controller');
+
+// Execute the requested task
+$controller = JController::getInstance('Attachments');
+$controller->execute(JRequest::getCmd('task'));
+$controller->redirect();
