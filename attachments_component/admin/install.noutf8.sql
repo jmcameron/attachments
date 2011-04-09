b5f8aea56836f51e730cf2a5541dac7974d36b6d
Jonathan Cameron <jmcameron@gmail.com>
2011-04-09 01:52:13 +0000

    Updated.

--- attachments_component/admin/install.noutf8.sql
+++ attachments_component/admin/install.noutf8.sql
@@ -8,11 +8,12 @@ CREATE TABLE IF NOT EXISTS `#__attachments`
     `url` TEXT NOT NULL DEFAULT '',
     `uri_type` ENUM('file', 'url') DEFAULT 'file',
     `url_valid` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+    `url_relative` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
     `display_name` VARCHAR(240) NOT NULL DEFAULT '',
     `description` TEXT NOT NULL DEFAULT '',
     `icon_filename` VARCHAR(60) NOT NULL,
     `uploader_id` INT(11) NOT NULL,
-    `published` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+    `state` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
     `user_field_1` TEXT NOT NULL DEFAULT '',
     `user_field_2` TEXT NOT NULL DEFAULT '',
     `user_field_3` TEXT NOT NULL DEFAULT '',
@@ -22,6 +23,8 @@ CREATE TABLE IF NOT EXISTS `#__attachments`
     `create_date` DATETIME DEFAULT NULL,
     `modification_date` DATETIME DEFAULT NULL,
     `download_count` INT(11) UNSIGNED DEFAULT '0',
+    `params` TEXT NOT NULL DEFAULT '',
+
      PRIMARY KEY (`id`)
 );
 
