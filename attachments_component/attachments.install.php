e5130fedfe5faba06825f9b6be2e867d08b08b42
Jonathan Cameron <jmcameron@gmail.com>
2011-04-24 00:22:10 +0000

    Upgraded package installation.

--- attachments_component/attachments.install.php
+++ attachments_component/attachments.install.php
@@ -4,6 +4,8 @@
 
 class Com_AttachmentsInstallerScript {
 
+	var $moved_attachments_dir = null;
+
 	var $plugins = Array('plg_content_attachments',
 						 'plg_search_attachments',
 						 'plg_attachments_plugin_framework',
@@ -13,21 +15,47 @@ class Com_AttachmentsInstallerScript {
 						 'plg_system_show_attachments_in_editor');
 
 
-	function install($parent) {
-		echo JText::_('ATTACHMENTS_COMPONENT_SUCCESFULLY_INSTALLED');
+	function install($parent)
+	{
+		$app = JFactory::getApplication('administrator');
+		$app->enqueueMessage(JText::sprintf('ATTACHMENTS_COMPONENT_SUCCESFULLY_INSTALLED'), 'message');
+		$app->enqueueMessage('<br/>', 'message');
 	}
 
-	function uninstall($parent) {
+	function uninstall($parent)
+	{
 	}
 
-	function update($parent) {
+	function update($parent)
+	{
 	}
 
-	function preflight($type, $parent) {
+	function preflight($type, $parent)
+	{
+		// Load the installation language
+		$lang =&  JFactory::getLanguage();
+		$lang->load('com_attachments.sys', dirname(__FILE__));
+
+		// Temporarily move the attachments directory out of the way to avoid conflicts
+		jimport('joomla.filesystem.folder');
+		$attachdir = JPATH_ROOT . DS . 'attachments';
+		if ( JFolder::exists($attachdir) ) {
+
+			// Move the attachments directory out of the way temporarily
+			$this->moved_attachments_dir = JPATH_ROOT. DS . 'temporarily_renamed_attachments_folder';
+			// ?? SHOULD Catch errors here
+			JFolder::move($attachdir, $this->moved_attachments_dir);
+
+			$app = JFactory::getApplication('administrator');
+			$msg = JText::sprintf('TEMPORARILY_RENAMED_ATTACHMENTS_DIR_TO_S', $this->moved_attachments_dir);
+			$app->enqueueMessage($msg, 'message');
+			$app->enqueueMessage('<br/>', 'message');
+			}
 	}
 
-	function postflight($type, $parent) {
 
+	function postflight($type, $parent) 
+	{
 		$app = JFactory::getApplication('administrator');
 		$db =& JFactory::getDBO();
 
@@ -60,5 +88,13 @@ class Com_AttachmentsInstallerScript {
 		$app->enqueueMessage('<br/>', 'message');
 		$app->enqueueMessage(JText::_('ALL_ATTACHMENTS_PLUGINS_ENABLED'), 'message');
 		$app->enqueueMessage('<br/>', 'message');
+
+		// Restore the attachments directory (if renamed)
+		if ( $this->moved_attachments_dir AND JFolder::exists($this->moved_attachments_dir) ) {
+			$attachdir = JPATH_ROOT . DS . 'attachments';
+			JFolder::move($this->moved_attachments_dir, $attachdir);
+			$app->enqueueMessage(JText::sprintf('RESTORED_ATTACHMENTS_DIR_TO_S', $attachdir), 'message');
+			$app->enqueueMessage('<br/>', 'message');
+			}
 	}
 }
