c94f6a973d1609534343287ab6ffc71f735e6d22
Jonathan Cameron <jmcameron@gmail.com>
2011-08-28 22:18:00 +0000

    Updated to release version number: 3.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = "3.0-RC3"
+VERSION = "3.0"
 VERSION2 = $(shell echo $(VERSION)|sed 's/ /-/g')
 ZIPFILE = attachments-$(VERSION2).zip
 
--- attachments_component/site/defines.php
+++ attachments_component/site/defines.php
@@ -23,7 +23,7 @@ class AttachmentsDefines
 {
 	/** The Attachments extension version number
 	 */
-	static $ATTACHMENTS_VERSION = '3.0-RC3';
+	static $ATTACHMENTS_VERSION = '3.0';
 
 	/** Project URL
 	 */
