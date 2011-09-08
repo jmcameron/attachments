10ac5181a49818938cdb6d2b4fc7103373a1786d
Jonathan Cameron <jmcameron@gmail.com>
2011-09-08 19:29:54 +0000

    Updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -16,6 +16,10 @@ defined('_JEXEC') or die('Restricted access');
 <pre>
 Attachments 3.x Extension for Joomla 1.6/1.7+
 
+2011-09-??: Release 3.0.1
+   - Fixed bug in migration import code that prevented proper error messages
+     when imports failed (eg, file not found, etc).
+
 2011-08-28: Release 3.0
    - Derived from unreleased 2.3
    - Significant refactoring, reimplementing, new features, cleanups, etc.
