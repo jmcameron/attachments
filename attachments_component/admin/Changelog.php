ac10944ce23fca0a89198a1aa12f7b8d4b3e97a1
Jonathan Cameron <jmcameron@gmail.com>
2011-09-08 19:30:19 +0000

    Updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -16,9 +16,12 @@ defined('_JEXEC') or die('Restricted access');
 <pre>
 Attachments 3.x Extension for Joomla 1.6/1.7+
 
-2011-09-??: Release 3.0.1
+2011-09-09: Release 3.0.1
+   - 2011-09-08 Added Save+New button in admin form to add attachments
+   - 2011-09-07 Fixed error in token IDs for admin publish/unpublish messages
    - 2011-09-06 Fixed bug in migration import code that prevented proper error
-     messages when imports failed (eg, file not found, etc).
+     messages when imports fail (eg, file not found, etc).  Changed the dry_run
+     success message to show number of attachments found in CSV file
 
 2011-08-28: Release 3.0
    - Derived from unreleased 2.3
