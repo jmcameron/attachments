d42c861080a6d065f973188cde71fce237ce0920
Jonathan Cameron <jmcameron@gmail.com>
2012-03-11 15:49:38 +0000

    Updated to work in Joomla 2.5.

--- attachments_search/attachments.php
+++ attachments_search/attachments.php
@@ -45,7 +45,7 @@ class plgSearchAttachments extends JPlugin
 	public function onContentSearchAreas()
 	{
 		static $areas = array(
-			'attachments' => JText::_('ATTACH_ATTACHMENTS')
+			'attachments' => 'ATTACH_ATTACHMENTS'
 			);
 		return $areas;
 	}
