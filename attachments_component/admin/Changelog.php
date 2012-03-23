88d45beeeb6b8dff25877e3d9ed777898e0b2633
Jonathan Cameron <jmcameron@gmail.com>
2012-03-23 17:40:22 +0000

    Updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -29,13 +29,15 @@ Attachments 3.x Extension for Joomla 1.7+
    - 2012-02-23 Cleaned up quoting in DB calls.
    - 2012-03-08 Added jimports for JController to a few files.
    - 2012-03-09 IE-specific fix for downloading filenames with special
-     characters in Internet Explorer.  Thanks to crassus168
-     (chris@gamehit.net) for suggestions for this fix.
+                characters in Internet Explorer.  Thanks to crassus168
+                (chris@gamehit.net) for suggestions for this fix.
    - 2012-03-10 Fixed handling of showing attachments for editing articles
-     from category blog and category list.
+                from category blog and category list.
    - 2012-03-10 Updated minor translation fix to search plugin code to resolve issues in Joomla 2.5.
    - 2012-03-19 Fixed issue with displayString refactor (front end upload failing).
    - 2012-03-23 Fixed bug in sorting by Creator name in backend attachments list.
+   - 2012-03-23 Fixed issue with redisplay after editing/deleting attachments from
+	            category blog view on front end.
 
 2011-09-17: Release 3.0.2
    - 2011-09-12 Improved display of frontend upload/update forms
