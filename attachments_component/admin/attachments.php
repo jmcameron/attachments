92df81a9c605b7c445262812a483b2113c77a246
Jonathan Cameron <jmcameron@gmail.com>
2012-07-23 20:07:18 +0000

    Minor update.

--- attachments_component/admin/attachments.php
+++ attachments_component/admin/attachments.php
@@ -5,7 +5,7 @@
  * @package Attachments
  * @subpackage Attachments_Component
  *
- * @copyright Copyright (C) 2007-2011 Jonathan M. Cameron, All Rights Reserved
+ * @copyright Copyright (C) 2007-2012 Jonathan M. Cameron, All Rights Reserved
  * @license http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
  * @link http://joomlacode.org/gf/project/attachments/frs/
  * @author Jonathan M. Cameron
@@ -13,7 +13,7 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-// Access check.
+// Access check
 if (!JFactory::getUser()->authorise('core.manage', 'com_attachments')) {
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR') . ' (ERR 28)');
 }
