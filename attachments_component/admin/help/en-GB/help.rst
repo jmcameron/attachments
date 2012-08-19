6b52b035671d644f5adfa74b4018fb60e8c6f152
Jonathan Cameron <jmcameron@gmail.com>
2012-08-19 02:21:13 +0000

    Updated version number.

--- attachments_component/admin/help/en-GB/help.rst
+++ attachments_component/admin/help/en-GB/help.rst
@@ -6,7 +6,7 @@
 
 .. class:: version
 
-**Version 3.0.1-Alpha1 - August 18, 2012**
+**Version 3.1-Alpha1 - August 18, 2012**
 
 **This version is for Joomla 1.7/2.5+**
 
--- attachments_component/site/defines.php
+++ attachments_component/site/defines.php
@@ -23,7 +23,7 @@ class AttachmentsDefines
 {
 	/** The Attachments extension version number
 	 */
-	static $ATTACHMENTS_VERSION = '3.0.1-Alpha1';
+	static $ATTACHMENTS_VERSION = '3.1-Alpha1';
 
 	/** Project URL
 	 */
