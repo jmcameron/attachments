2539b6f69722c518ca4e978462b1a82ad5c2d505
Jonathan Cameron <jmcameron@gmail.com>
2012-09-07 17:05:45 +0000

    updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -16,9 +16,8 @@ defined('_JEXEC') or die('Restricted access');
 <pre>
 Attachments 3.x Extension for Joomla 1.7/2.5+
 
-2012-??: Release 3.1
-   - 2012-08-18 Added attachments list sort option: description (reversed, z-a)
-   - 2012-09-07 Fixed bug causing crashes when component (only) is uninstalled
+2012-09-07: Release 3.0.4
+   - 2012-09-07 Fixed bug causing crashes when component (only) is uninstalled.
 
 2012-08-11: Release 3.0.3
    - 2011-09-27 Fixed errors in the English version of the help file
