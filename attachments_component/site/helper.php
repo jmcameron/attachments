1807f6e5b1f47fe2cc40750800afee2724dbd6d1
Jonathan Cameron <jmcameron@gmail.com>
2012-12-17 00:58:18 +0000

    Added 'javascript.php' file for AttachmentsJavascript class to handle most Attachments javascript functionality.

--- attachments_component/site/helper.php
+++ attachments_component/site/helper.php
@@ -25,54 +25,6 @@ require_once(JPATH_SITE.'/components/com_attachments/defines.php');
 class AttachmentsHelper
 {
 	/**
-	 * Set up the appropriate Javascript framework (Mootools or jQuery)
-	 */
-	public static function setupJavascript($add_refresh_script = true)
-	{
-		// up the style sheet (to get the visual for the button working)
-		JHTML::_('behavior.mootools');
-		if ($add_refresh_script)
-		{
-			$uri = JFactory::getURI();
-			$base_url = $uri->root(true);
-			$js_path = $base_url . '/plugins/content/attachments/attachments_refresh.js';
-			$doc = JFactory::getDocument();
-			$doc->addScript( $js_path );
-		}
-	}
-
-
-	/**
-	 * Close the iframe
-	 */
-	public static function closeIframeRefreshAttachments($base_url, $parent_type, $parent_entity, $parent_id, $from)
-	{
-		echo "<script type=\"text/javascript\">
-			window.parent.refreshAttachments(\"$base_url\",\"$parent_type\",\"$parent_entity\",$parent_id,\"$from\");
-			window.parent.SqueezeBox.close();
-			</script>";
-	}
-
-
-	/**
-	 * Set up the Javascript for the modal button
-	 */
-	public static function setupModalJavascript()
-	{
-		JHTML::_('behavior.modal', 'a.modal-button');
-	}
-
-
-	/**
-	 * Close the modal window and reload the parent
-	 */
-	public static function closeModal()
-	{
-		echo '<script>var myparent = window.parent; window.parent.SqueezeBox.close(); myparent.location.reload();</script>';
-	}
-
-
-	/**
 	 * Install the specified stylesheet (handles caching)
 	 *
 	 * @param string $css_path the URL/path to the CSS file to add to the document
