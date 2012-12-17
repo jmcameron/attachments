c721c2f7b1cdc95b6781a69cd511ee44b57d3b9b
Jonathan Cameron <jmcameron@gmail.com>
2012-12-17 05:24:47 +0000

    Converted all occurences of JHTML to JHtml.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -58,6 +58,7 @@ Attachments 3.x Extension for Joomla 1.7/2.5+
    - 2012-12-05 Implemented fix for downloading large files by send the file in 8K chunks.
    - 2012-12-16 Various updates, fixes, updates and tweaks for Joomla 3.0 compatibility.
 		(This is a work in progress; still some rough edges.)
+   - 2012-12-16 Switched all JHTML to JHtml everywhere.
 
 2012-09-07: Release 3.0.4
    - 2012-09-07 Fixed bug causing crashes when component (only) is uninstalled.
