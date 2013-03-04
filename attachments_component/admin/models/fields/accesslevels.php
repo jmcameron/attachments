0a85eada986e88f770e5eb3ffafac96d554a9347
Jonathan Cameron <jmcameron@gmail.com>
2013-03-04 01:26:20 +0000

    Added new option to control display of attachments to non logged-in users (guests).

--- attachments_component/admin/models/fields/accesslevels.php
+++ attachments_component/admin/models/fields/accesslevels.php
@@ -46,7 +46,11 @@ class JFormFieldAccessLevels extends JFormField
 	 */
 	protected function getInput()
 	{
-		return $this->getAccessLevels($this->name, 'jform_'.$this->fieldname, $this->value);
+		$options = new JObject();
+		$options->element = $this->element;
+		$options->multiple = $this->multiple;
+		$options->always_public = $this->fieldname == 'show_guest_access_levels';
+		return $this->getAccessLevels($this->name, 'jform_'.$this->fieldname, $this->value, $options);
 	}
 
 
@@ -55,9 +59,9 @@ class JFormFieldAccessLevels extends JFormField
 	 *
 	 * @param string $for_id the id for the select input
 	 * @param string $fieldname the name of the field
-	 * @param int $level_value the value of the level to be initially selected
+	 * @param int $level_value the value of the level(s) to be initially selected
 	 */
-	public static function getAccessLevels($for_id, $fieldname, $level_value=null)
+	public static function getAccessLevels($for_id, $fieldname, $level_value=null, $options=null)
 	{
 		$user	= JFactory::getUser();
 		$user_access_levels = array_unique($user->getAuthorisedViewLevels());
@@ -81,6 +85,7 @@ class JFormFieldAccessLevels extends JFormField
 			$errmsg = $db->stderr() . ' (ERR 115)';
 			JError::raiseError(500, $errmsg);
 			}
+		$num_levels = (int)count($levels);
 
 		// Make sure there is a $level_value
 		if ( $level_value === null ) {
@@ -90,27 +95,52 @@ class JFormFieldAccessLevels extends JFormField
 			}
 
 		// Make sure the $level_value is in the user's authorised levels
-		if ( !in_array($level_value, $user_access_levels) ) {
-			// If not, set $level_value to the lowest legal value
-			$registered = 2;
-			if ( in_array($registered, $user_access_levels) ) {
-				$level_value = $registered;
-				}
-			else {
-				$sorted_access_levels = sort($user_access_levels, SORT_NUMERIC);
-				$level_value = $sorted_access_levels[0];
+		if (!is_array($level_value)) {
+			if ( !in_array($level_value, $user_access_levels) ) {
+				// If not, set $level_value to the lowest legal value
+				$registered = 2;
+				if ( in_array($registered, $user_access_levels) ) {
+					$level_value = $registered;
+					}
+				else {
+					$sorted_access_levels = sort($user_access_levels, SORT_NUMERIC);
+					$level_value = $sorted_access_levels[0];
+					}
 				}
 			}
 
-		// Construct the drop-down list
+		// Make sure Public is always selected, if desired
+		if (is_array($level_value) AND ($options !== null) AND $options->always_public) {
+			if ( !in_array('1', $level_value) ) {
+				array_unshift($level_value, '1');
+				}
+			}
+		else {
+			if ( $level_value != '1' ) {
+				$level_value = Array('1', $level_value);
+				}
+			}
+
+		// Construct the attributes for the list
+		$attr = '';
+		if ( $options === null ) {
+			$attr = 'class="inputbox" size="1"';
+			}
+		else {
+			$attr .= $options->element['class'] ? ' class="' . (string) $options->element['class'] . '"' : '';
+			$attr .= ((string) $options->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+			$attr .= $options->element['size'] ? ' size="' . (int) $options->element['size'] . '"' : '';
+			$attr .= $options->multiple ? ' multiple="multiple"' : '';
+			}
+			
+		// Construct the list
 		$level_options = Array();
 		foreach ( $levels as $level ) {
 			// NOTE: We do not translate the access level titles
 			$level_options[] = JHtml::_('select.option', $level->id, $level->title);
 			}
 		return JHtml::_('select.genericlist',  $level_options, $for_id,
-						'class="inputbox" size="1"', 'value', 'text', $level_value,
-						$fieldname
+						$attr, 'value', 'text', $level_value, $fieldname
 						);
 	}
 }
