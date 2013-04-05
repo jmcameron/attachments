168dc7b15bd6868ee5de5e687d0c9c49896e73b1
Jonathan Cameron <jmcameron@gmail.com>
2013-04-05 20:49:46 +0000

    Updated version number.

--- attachments_component/site/defines.php
+++ attachments_component/site/defines.php
@@ -23,7 +23,7 @@ class AttachmentsDefines
 {
 	/** The Attachments extension version number
 	 */
-	static $ATTACHMENTS_VERSION = '3.1-Beta3';
+	static $ATTACHMENTS_VERSION = '3.1-Beta4';
 
 	/** Project URL
 	 */
