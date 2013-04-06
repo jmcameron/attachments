8a8f470ecef6cbab5c7ef68c34b315cfc629d9a7
Jonathan Cameron <jmcameron@gmail.com>
2013-04-06 21:12:55 +0000

    updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -82,6 +82,7 @@ Attachments 3.x Extension for Joomla 2.5+
    - 2012-04-05 Refactored css/js files to use central media/com_attachments folder.
    - 2012-04-05 Fixed bug that was exposing registered attachments in non-secure mode.
         (Introduced in the 2013-03-01 mod to display links for non-public attachments.)
+   - 2012-04-05 Minor cosmetic adjustments to form displays in frontend and backend.
 
 2012-09-07: Release 3.0.4
    - 2012-09-07 Fixed bug causing crashes when component (only) is uninstalled.
