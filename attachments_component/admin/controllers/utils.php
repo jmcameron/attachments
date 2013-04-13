0600ce2f3cf856259fa2e2a6c61e54f619405d9d
Jonathan Cameron <jmcameron@gmail.com>
2013-04-13 06:47:34 +0000

    Removed unnecessary import.

--- attachments_component/admin/controllers/utils.php
+++ attachments_component/admin/controllers/utils.php
@@ -18,8 +18,6 @@ if (!JFactory::getUser()->authorise('core.admin', 'com_attachments')) {
 	return JError::raiseError(404, JText::_('JERROR_ALERTNOAUTHOR') . ' (ERR 149)');
 	}
 
-jimport('joomla.application.component.controller');
-
 /** Define the legacy classes, if necessary */
 require_once(JPATH_SITE.'/components/com_attachments/legacy/controller.php');
 
