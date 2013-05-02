bf468ce331085154b198cceb57bce109e4e79457
Jonathan Cameron <jmcameron@gmail.com>
2013-05-02 06:44:17 +0000

    Removed comment.

--- attachments_component/admin/sql/updates/mysql/3.1-2012-11-17.sql
+++ attachments_component/admin/sql/updates/mysql/3.1-2012-11-17.sql
@@ -1,4 +1,2 @@
-/* Increase allowed filename size to 256 characters */
-
 ALTER TABLE `#__attachments` MODIFY `filename` VARCHAR(256) NOT NULL;
 ALTER TABLE `#__attachments` MODIFY `filename_sys` VARCHAR(512) NOT NULL;
