0a7fed3893a31c956661abab1a00767f11244053
Jonathan Cameron <jmcameron@gmail.com>
2013-05-04 08:50:20 +0000

    Updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -92,6 +92,8 @@ Attachments 3.x Extension for Joomla 2.5+
 		Other small tweaks to the CSS files for the 'add attachment' link.
 	- 2012-04-13 Added quickicon plugin for both Joomla 2.5 and 3.x.
 	- 2012-04-19 Converted help page to use a view with template and translation tokens.
+	- 2012-05-03 Refactored/cleaned up add/edit/update/upload views and controller/helper
+	   code that invokes the views.
 
 2012-09-07: Release 3.0.4
 	- 2012-09-07 Fixed bug causing crashes when component (only) is uninstalled.
