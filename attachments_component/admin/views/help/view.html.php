ec6f04114a437b91550d16726fbaa050707cf51f
Jonathan Cameron <jmcameron@gmail.com>
2013-05-21 06:21:26 +0000

    Cleaned up to comply with Joomla coding standards.

--- attachments_component/admin/views/help/view.html.php
+++ attachments_component/admin/views/help/view.html.php
@@ -2,29 +2,30 @@
 /**
  * Attachments component
  *
- * @package Attachments
- * @subpackage Attachments_Component
+ * @package     Attachments
+ * @subpackage  Attachments_Component
  *
- * @copyright Copyright (C) 2007-2012 Jonathan M. Cameron, All Rights Reserved
- * @license http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
- * @link http://joomlacode.org/gf/project/attachments/frs/
- * @author Jonathan M. Cameron
+ * @author      Jonathan M. Cameron <jmcameron@jmcameron.net>
+ * @copyright   Copyright (C) 2007-2012 Jonathan M. Cameron, All Rights Reserved
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @link        http://joomlacode.org/gf/project/attachments/frs/
  */
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 
-/** Define the legacy classes, if necessary */
-require_once('helpview.php');
+// Define the legacy classes, if necessary
+require_once 'helpview.php';
 
-/** Load the Attachments defines */
-require_once(JPATH_SITE.'/components/com_attachments/defines.php');
+// Load the Attachments defines
+require_once JPATH_SITE . '/components/com_attachments/defines.php';
 
 
 /**
  * View for the help page
- * 
- * @package Attachments
+ *
+ * @package  Attachments
+ * @since    3.1
  */
 class AttachmentsViewHelp extends HelpView
 {
@@ -33,10 +34,11 @@ class AttachmentsViewHelp extends HelpView
 	 *
 	 * @param   string  $tpl  A template file to load. [optional]
 	 *
+	 * @return nothing
 	 */
 	public function display($tpl = null)
 	{
-		$this->logo_img = JHtml::image('com_attachments/attachments_help_logo32.png','',null, true);
+		$this->logo_img = JHtml::image('com_attachments/attachments_help_logo32.png', '', null, true);
 		$this->version = AttachmentsDefines::$ATTACHMENTS_VERSION;
 		$rdate = new JDate(AttachmentsDefines::$ATTACHMENTS_VERSION_DATE);
 		$this->date = JHtml::_('date', $rdate, JText::_('DATE_FORMAT_LC1'));
@@ -44,15 +46,16 @@ class AttachmentsViewHelp extends HelpView
 		parent::display($tpl);
 	}
 
-
 	/**
 	 * Add the start of the permissions table including the header
 	 *
-	 * @param  string  $col1_code  Language token for column 1 (permission name)
-	 * @param  string  $col2_code  Language token for column 2 (permission note)
-	 * @param  string  $col3_code  Language token for column 3 (permission action)
+	 * @param   string  $col1_code  Language token for column 1 (permission name)
+	 * @param   string  $col2_code  Language token for column 2 (permission note)
+	 * @param   string  $col3_code  Language token for column 3 (permission action)
+	 *
+	 * @return nothing
 	 */
-	function startPermissionsTable($col1_code, $col2_code, $col3_code)
+	protected function startPermissionsTable($col1_code, $col2_code, $col3_code)
 	{
 		echo "<table id=\"permissions\"class=\"permissions docutils\">\n";
 		echo "<colgroup>\n";
@@ -62,9 +65,9 @@ class AttachmentsViewHelp extends HelpView
 		echo "</colgroup>\n";
 		echo "<thead>\n";
 		echo "  <tr>\n";
-		echo "     <th class=\"head\">".JText::_($col1_code)."</th>\n";
-		echo "     <th class=\"head\">".JText::_($col2_code)."</th>\n";
-		echo "     <th class=\"head\">".JText::_($col3_code)."</th>\n";
+		echo "     <th class=\"head\">" . JText::_($col1_code) . "</th>\n";
+		echo "     <th class=\"head\">" . JText::_($col2_code) . "</th>\n";
+		echo "     <th class=\"head\">" . JText::_($col3_code) . "</th>\n";
 		echo "  </tr>\n";
 		echo "</thead>\n";
 		echo "<tbody>\n";
@@ -73,26 +76,28 @@ class AttachmentsViewHelp extends HelpView
 	/**
 	 * Add the a row of the permissions table
 	 *
-	 * @param  string  $col1_code  Language token for column 1 (permission name)
-	 * @param  string  $col2_code  Language token for column 2 (permission note)
-	 * @param  string  $col3_code  Language token for column 3 (permission action)
+	 * @param   string  $col1_code  Language token for column 1 (permission name)
+	 * @param   string  $col2_code  Language token for column 2 (permission note)
+	 * @param   string  $col3_code  Language token for column 3 (permission action)
+	 *
+	 * @return nothing
 	 */
-	function addPermissionsTableRow($col1_code, $col2_code, $col3_code)
+	protected function addPermissionsTableRow($col1_code, $col2_code, $col3_code)
 	{
 		echo "  <tr>\n";
-		echo "     <td>".JText::_($col1_code)."</td>\n";
-		echo "     <td>".JText::_($col2_code)."</td>\n";
-		echo "     <td>".JText::_($col3_code)."</td>\n";
+		echo "     <td>" . JText::_($col1_code) . "</td>\n";
+		echo "     <td>" . JText::_($col2_code) . "</td>\n";
+		echo "     <td>" . JText::_($col3_code) . "</td>\n";
 		echo "  </tr>\n";
 	}
 
-
 	/**
 	 * Add the end of the permissions table
+	 *
+	 * @return nothing
 	 */
-	function endPermissionsTable()
+	protected function endPermissionsTable()
 	{
 		echo "</table>\n";
 	}
-
 }
