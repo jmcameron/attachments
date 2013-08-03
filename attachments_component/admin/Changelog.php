9378cfd59d5b4665a03db212b8a89663dd3e1bcb
Jonathan Cameron <jmcameron@gmail.com>
2013-08-03 01:30:29 +0000

    Updated.

--- attachments_component/admin/Changelog.php
+++ attachments_component/admin/Changelog.php
@@ -30,6 +30,7 @@ Attachments 3.x Extension for Joomla 2.5+
         Changed 'KB' to 'kB' to match Wikipedia 'kilobyte' article
     - 2013-07-30 Minor fixes for robustness
     - 2013-07-31 Added fix to display attachments for non-menu category blog views.
+	- 2013-08-02 Fix to return user logging in to the prior page.
 
 2013-07-11: Release 3.1.1
     - 2013-07-11 Prevent uploading image file exploits (Security Fix for VEL)
