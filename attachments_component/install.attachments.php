442d43ddffade80185264dbf6a3a882374058fdf
Jonathan Cameron <jmcameron@gmail.com>
2014-09-15 03:41:06 +0000

    Merge branch 'installs'

--- attachments_component/install.attachments.php
+++ attachments_component/install.attachments.php
@@ -107,27 +107,20 @@ class com_AttachmentsInstallerScript {
 	 */
 	public function uninstall($parent)
 	{
-		$app = JFactory::getApplication();
-		$db = JFactory::getDBO();
-
-		// Make sure the translations are available
-		$lang = JFactory::getLanguage();
-		$lang->load('com_attachments', JPATH_ADMINISTRATOR);
-
 		// disable all the plugins
 		foreach ($this->plugins as $plugin_name)
 		{
 			// Make the query to enable the plugin
-			$plugin_title = JText::_($plugin_name);
+			$db = JFactory::getDBO();
 			$query = $db->getQuery(true);
-			$query->update('#__extensions');
-			$query->set("enabled = 0");
-			$query->where('type=' . $db->quote('plugin') . ' AND name=' . $db->quote($plugin_name));
+			$query->update('#__extensions')
+			      ->set("enabled = 0")
+			      ->where('type=' . $db->quote('plugin') . ' AND name=' . $db->quote($plugin_name));
 			$db->setQuery($query);
 			$db->query();
 
-			// Do NOT complain if there was an error
-			// (in case any plugins are already uninstalled and this query fails)
+			// NOTE: Do NOT complain if there was an error
+			// (in case any plugin is already uninstalled and this query fails)
 		}
 	}
 
