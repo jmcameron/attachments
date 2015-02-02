4475497b88f572e3ec221ab14b55dbea7b43c95c
Jonathan Cameron <jmcameron@gmail.com>
2015-02-01 19:47:10 -0800

    Updated version

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = "3.1.4-Beta14"
+VERSION = "3.2.0"
 VERSION2 = $(shell echo $(VERSION)|sed 's/ /-/g')
 ZIPFILE = attachments-$(VERSION2).zip
 
--- attachments_component/site/defines.php
+++ attachments_component/site/defines.php
@@ -25,11 +25,11 @@ class AttachmentsDefines
 {
 	/** The Attachments extension version number
 	 */
-	public static $ATTACHMENTS_VERSION = '3.1.4-Beta14';
+	public static $ATTACHMENTS_VERSION = '3.2.0';
 
 	/** The Attachments extension version date
 	 */
-	public static $ATTACHMENTS_VERSION_DATE = 'January 24, 2015';
+	public static $ATTACHMENTS_VERSION_DATE = 'February  1, 2015';
 
 	/** Project URL
 	 */
