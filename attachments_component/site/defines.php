36c63af2397cc730b824d6e9f5378a3d871436f3
Jonathan Cameron <jmcameron@gmail.com>
2015-02-01 20:19:22 -0800

    Fixed date format

--- attachments_component/site/defines.php
+++ attachments_component/site/defines.php
@@ -29,7 +29,7 @@ class AttachmentsDefines
 
 	/** The Attachments extension version date
 	 */
-	public static $ATTACHMENTS_VERSION_DATE = 'February  1, 2015';
+	public static $ATTACHMENTS_VERSION_DATE = 'February 1, 2015';
 
 	/** Project URL
 	 */
