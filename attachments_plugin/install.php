b7350278c35c0ee22f14ad0f251e68ad42f813a8
Jonathan Cameron <jmcameron@gmail.com>
2017-04-30 21:13:51 -0700

    Updated copyright notices

--- attachments_plugin/install.php
+++ attachments_plugin/install.php
@@ -6,7 +6,7 @@
  * @subpackage Attachments_Plugin
  *
  * @author Jonathan M. Cameron
- * @copyright Copyright (C) 2014-2016 Jonathan M. Cameron
+ * @copyright Copyright (C) 2014-2017 Jonathan M. Cameron
  * @license http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
  * @link http://joomlacode.org/gf/project/attachments/frs/
  */
