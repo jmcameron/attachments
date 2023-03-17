500cedc7f16c33753a2982a33ecddd21d67cd623
Theofilos Intzoglou <int.teo@gmail.com>
2023-03-17 16:14:09 +0200

    Replace deprecated code

--- insert_attachments_token_btn_plugin/insert_attachments_token.php
+++ insert_attachments_token_btn_plugin/insert_attachments_token.php
@@ -11,17 +11,24 @@
  * @author Jonathan M. Cameron
  */
 
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
+
 // no direct access
 defined( '_JEXEC' ) or die('Restricted access');
-
-jimport('joomla.plugin.plugin');
 
 /**
  * Button that allows you to insert an {attachments} token into the text from the editor
  *
  * @package Attachments
  */
-class plgButtonInsert_attachments_token extends JPlugin
+class plgButtonInsert_attachments_token extends CMSPlugin
 {
 	/**
 	 * Constructor
@@ -42,15 +49,14 @@ class plgButtonInsert_attachments_token extends JPlugin
 	 *
 	 * @param string $name The name of the editor form
 	 * @param int $asset The asset ID for the entity being edited
-	 * @param int $authro The ID of the author of the entity
+	 * @param int $author The ID of the author of the entity
 	 *
 	 * @return a button
 	 */
 	public function onDisplay($name, $asset, $author)
 	{
 		// Get the component parameters
-		jimport('joomla.application.component.helper');
-		$params = JComponentHelper::getParams('com_attachments');
+		$params = ComponentHelper::getParams('com_attachments');
 
 		// This button should only be displayed in 'custom placement' mode.
 		// Check to make sure that is the case
@@ -60,7 +66,8 @@ class plgButtonInsert_attachments_token extends JPlugin
 			}
 
 		// Avoid displaying the button for anything except for registered parents
-		$parent_type = JRequest::getCmd('option');
+		$input = Factory::getApplication()->getInput();
+		$parent_type = $input->getCmd('option');
 
 		// Handle sections and categories specially (since they are really com_content)
 		if ($parent_type == 'com_categories') {
@@ -68,26 +75,25 @@ class plgButtonInsert_attachments_token extends JPlugin
 			}
 
 		// Get the article/parent handler
-		JPluginHelper::importPlugin('attachments');
+		PluginHelper::importPlugin('attachments');
 		$apm = getAttachmentsPluginManager();
 		if ( !$apm->attachmentsPluginInstalled($parent_type) ) {
 			// Exit if there is no Attachments plugin to handle this parent_type
-			return new JObject();
+			return new stdClass();
 			}
 
 		// Get ready for language things
-		$lang =	 JFactory::getLanguage();
+		$lang =	 Factory::getApplication()->getLanguage();
 		if ( !$lang->load('plg_editors-xtd_insert_attachments_token', dirname(__FILE__)) ) {
 			// If the desired translation is not available, at least load the English
 			$lang->load('plg_editors-xtd_insert_attachments_token', JPATH_ADMINISTRATOR, 'en-GB');
 			}
 
 		// Set up the Javascript to insert the tag
-		$getContent = $this->_subject->getContent($name);
-		$present = JText::_('ATTACH_ATTACHMENTS_TOKEN_ALREADY_PRESENT', true) ;
+		$present = Text::_('ATTACH_ATTACHMENTS_TOKEN_ALREADY_PRESENT', true) ;
 		$js =  "
 			function insertAttachmentsToken(editor) {
-				var content = $getContent
+				var content = Joomla.editors.instances['$name'].getValue();
 				if (content.match(/\{\s*attachments/i)) {
 					alert('$present');
 					return false;
@@ -97,43 +103,31 @@ class plgButtonInsert_attachments_token extends JPlugin
 			}
 			";
 
-		$app = JFactory::getApplication();
-		$doc = JFactory::getDocument();
-		$uri = JFactory::getURI();
+		$app = Factory::getApplication();
+		$doc = $app->getDocument();
+		$uri = Uri::getInstance();
 
 		$doc->addScriptDeclaration($js);
 
 		// Add the regular css file
-		JHtml::stylesheet('com_attachments/attachments_list.css', Array(), true);
-		JHtml::stylesheet('com_attachments/insert_attachments_token_button.css', Array(), true);
+		HTMLHelper::stylesheet('com_attachments/attachments_list.css', Array(), true);
+		HTMLHelper::stylesheet('com_attachments/insert_attachments_token_button.css', Array(), true);
 
 		// Handle RTL styling (if necessary)
 		if ( $lang->isRTL() ) {
-			JHtml::stylesheet('com_attachments/attachments_list_rtl.css', Array(), true);
-			JHtml::stylesheet('com_attachments/insert_attachments_token_button_rtl.css', Array(), true);
+			HTMLHelper::stylesheet('com_attachments/attachments_list_rtl.css', Array(), true);
+			HTMLHelper::stylesheet('com_attachments/insert_attachments_token_button_rtl.css', Array(), true);
 			}
 
-		$button = new JObject();
+		$button = new Registry();
 		$button->set('modal', false);
 		$button->set('class', 'btn');
 		$button->set('onclick', 'insertAttachmentsToken(\''.$name.'\');return false;');
-		$button->set('text', JText::_('ATTACH_ATTACHMENTS_TOKEN'));
-		$button->set('title', JText::_('ATTACH_ATTACHMENTS_TOKEN_DESCRIPTION'));
+		$button->set('text', Text::_('ATTACH_ATTACHMENTS_TOKEN'));
+		$button->set('title', Text::_('ATTACH_ATTACHMENTS_TOKEN_DESCRIPTION'));
 
-		if ( $app->isAdmin() ) {
-			$button_name = 'insert_attachments_token';
-			if (version_compare(JVERSION, '3.3', 'ge')) {
-				$button_name = 'paperclip';
-				}
-			$button->set('name', $button_name);
-			}
-		else {
-			$button_name = 'insert_attachments_token_frontend';
-			if (version_compare(JVERSION, '3.3', 'ge')) {
-				$button_name = 'paperclip';
-				}
-			$button->set('name', $button_name);
-			}
+		$button_name = 'paperclip';
+		$button->set('name', $button_name);
 
 		// TODO: The button writer needs to take into account the javascript directive
 		// $button->set('link', 'javascript:void(0)');
