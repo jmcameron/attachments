e54977b893f81057e3f5819cd6daad4b648736e6
JLTRY <jltryoen@orange.fr>
2025-07-12 16:26:36 +0200

    deliver version 4.1.5

--- attachments_component/site/src/Helper/AttachmentsDefines.php
+++ attachments_component/site/src/Helper/AttachmentsDefines.php
@@ -30,11 +30,11 @@ class AttachmentsDefines
 {
     /** The Attachments extension version number
      */
-    public static $ATTACHMENTS_VERSION = '4.1.4.4';
+    public static $ATTACHMENTS_VERSION = '4.1.5';
 
     /** The Attachments extension version date
      */
-    public static $ATTACHMENTS_VERSION_DATE = 'July  6, 2025';
+    public static $ATTACHMENTS_VERSION_DATE = 'July 12, 2025';
 
     /** Project URL
      */
