350d2763c1f6f15d93e83f89c140f470fcd151e9
JLTRY <jltryoen@orange.fr>
2026-01-06 14:36:55 +0100

    version 4.2.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = "4.2.0"
+VERSION = "4.2.1"
 VERSION2 = $(shell echo $(VERSION)|sed 's/ /-/g')
 ZIPFILE = attachments-$(VERSION2).zip
 
--- attachments_component/site/src/Helper/AttachmentsDefines.php
+++ attachments_component/site/src/Helper/AttachmentsDefines.php
@@ -30,11 +30,11 @@ class AttachmentsDefines
 {
     /** The Attachments extension version number
      */
-    public static $ATTACHMENTS_VERSION = '4.2.0';
+    public static $ATTACHMENTS_VERSION = '4.2.1';
 
     /** The Attachments extension version date
      */
-    public static $ATTACHMENTS_VERSION_DATE = 'November  7, 2025';
+    public static $ATTACHMENTS_VERSION_DATE = 'January  6, 2026';
 
     /** Project URL
      */
