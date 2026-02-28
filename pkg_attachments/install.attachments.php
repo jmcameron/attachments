4e148ea65d24e68608ec003229eaf66260a8ba34
Theofilos Intzoglou <int.teo@gmail.com>
2026-02-28 19:58:34 +0200

    Add a test if the joomla 6 compatibility plugin is enabled Fixes #188

--- pkg_attachments/install.attachments.php
+++ pkg_attachments/install.attachments.php
@@ -13,10 +13,11 @@
  */
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\Folder;
+use Joomla\Filesystem\Folder;
 use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\CMS\Installer\InstallerScriptInterface;
 use Joomla\CMS\Language\Text;
+use Joomla\Database\DatabaseInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -30,6 +31,24 @@ use Joomla\CMS\Language\Text;
 // phpcs:ignore
 class pkg_AttachmentsInstallerScript implements InstallerScriptInterface
 {
+    // Check if the compatibility plugin is enabled
+    private function isCompatibilityPluginEnabled(): bool
+    {
+        $db = Factory::getContainer()
+            ->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->select($db->quoteName('enabled'))
+            ->from($db->quoteName('#__extensions'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
+            ->where($db->quoteName('element') . ' = ' . $db->quote('compatibility'))  // plugin name
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'));          // plugin group
+
+        $db->setQuery($query);
+
+        return (bool) $db->loadResult();
+    }
+
+
     /**
      * Attachments component install function
      *
@@ -76,6 +95,16 @@ class pkg_AttachmentsInstallerScript implements InstallerScriptInterface
     public function preflight(string $type, InstallerAdapter $adapter): bool
     {
         $app = Factory::getApplication();
+
+        if (
+            version_compare(JVERSION, '6', 'ge') &&
+            !$this->isCompatibilityPluginEnabled()
+        ) {
+            $msg = Text::_('ATTACH_PACKAGE_NEEDS_COMPATIBILITY_PLUGIN');
+            $app->enqueueMessage($msg, 'warning');
+
+            return false;
+        }
 
         // Load the installation language
         $lang = $app->getLanguage();
